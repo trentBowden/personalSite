695be0703c1314989dcbe1d5c985b10f87046f51
Trent Bowden <a1672582>
2018-04-02 10:22:38 +0930

    changed topnav

--- scripts.js
+++ scripts.js
@@ -6,22 +6,41 @@ Menu Code
 -Called on scroll, sticks navbar to top of page.
  */
 
-function responsiveMenu() {
-    var x = document.getElementById("topNav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+// function responsiveMenu() {
+//     var x = document.getElementById("topNav");
+//     console.log(x.classList);
+//     if (!x.classList.contains("responsive")) {
+//         x.classList.add("responsive");
+//         stickyScrollCheck();
+//     } else {
+//         x.classList.remove("responsive");
+//     }
+// }
+//
+// window.onscroll = function() {stickyScrollCheck()};
+// var navbar = document.getElementById("topNav");
+// var sticky = navbar.offsetTop;
+// function stickyScrollCheck() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+/////////////////// new below
+
+const nav = document.querySelector('#main');
+let topOfNav = nav.offsetTop;
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
     } else {
-        x.className = "topnav";
+        document.body.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
     }
 }
 
-window.onscroll = function() {stickyScrollCheck()};
-var navbar = document.getElementById("topNav");
-var sticky = navbar.offsetTop;
-function stickyScrollCheck() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+window.addEventListener('scroll', fixNav);
